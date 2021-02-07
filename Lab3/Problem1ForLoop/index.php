1ae0368360dcc301d87dc4dee0aad1d4c11c842f
SAMIUL98SIAM <samiulsiam89@gmail.com>
2021-02-07 16:58:22 +0600

    Loops 1 no problem has done

---
+++ Lab3/Problem1ForLoop/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>For Loop - 1</title>
+</head>
+<body>
+	<?php
+		$color = array('white', 'green', 'red', 'blue', 'black');
+	?>
+	<p>
+       The memory of that scene for me is like a frame of film forever frozen at that moment: the <?php echo "$color[2]"?> carpet, the <?php echo "$color[1]"?> lawn, the <?php echo "$color[0]"?> house, the leaden sky. The new president and his first lady. - Richard M. Nixon 
+    </p>
+</body>
+</html>
